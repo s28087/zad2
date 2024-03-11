d884ca324507cabb17130ea2a0617aa8f07ed6fb
s28087 <s28087@pjwstk.edu.pl>
2024-03-11 16:28:36 +0100

    Modyfikacja1

--- zad2/zad2/Program.cs
+++ zad2/zad2/Program.cs
@@ -1,3 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        // java: addStudent(Student stud){}
+        // c#: AddStudent(Student stud){}
+        //string nie String
+        
+        Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Hello, World!");
+        int[] tab = { 1, 2, 3, 4 };
+        
+        //sr arytm i max w tablicy
+
+
+        /*for (int i = 0; i < tab.Length; i++)
+        {
+            Console.WriteLine(i);
+        }*/
+        
+        
+        //////////////
+
+        //////////////
+        ///
+        ///
+        ///
+        ///
+        ///
+        ///
+        /// 
+        int srednia = 0;
+        
+        foreach (int num in tab)
+        {
+            srednia += num;
+        }
+
+        srednia = srednia / tab.Length;
+        
+        Console.WriteLine("Hello, World!");
+    }
+}
+
